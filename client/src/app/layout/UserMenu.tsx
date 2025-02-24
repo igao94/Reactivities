f6248b10ada1ecc35ccc7d79cf55205e861cb354
igao94 <igao998@gmail.com>
2025-02-24 09:36:51 +0100

    Added client-side photo upload

--- client/src/app/layout/UserMenu.tsx
+++ client/src/app/layout/UserMenu.tsx
@@ -34,7 +34,7 @@ export default function UserMenu() {
         sx={{ fontSize: "1.1rem" }}
       >
         <Box display="flex" alignItems="center" gap={2}>
-          <Avatar />
+          <Avatar src={currentUser?.imageUrl} alt="current user image" />
           {currentUser?.displayName}
         </Box>
       </Button>
@@ -53,7 +53,11 @@ export default function UserMenu() {
           </ListItemIcon>
           <ListItemText>Create activity</ListItemText>
         </MenuItem>
-        <MenuItem component={Link} to="/profile" onClick={handleClose}>
+        <MenuItem
+          component={Link}
+          to={`/profiles/${currentUser?.id}`}
+          onClick={handleClose}
+        >
           <ListItemIcon>
             <Person />
           </ListItemIcon>
