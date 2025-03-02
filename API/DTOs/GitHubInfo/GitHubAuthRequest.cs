8633122b78452b2eb41f4b68adc92dc1a9fae6cb
igao94 <igao998@gmail.com>
2025-03-02 13:59:51 +0100

    Added social login with GitHub

---
+++ API/DTOs/GitHubInfo/GitHubAuthRequest.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace API.DTOs.GitHubInfo;
+
+public class GitHubAuthRequest
+{
+    public required string Code { get; set; }
+
+    [JsonPropertyName("client_id")]
+    public required string ClientId { get; set; }
+
+    [JsonPropertyName("client_secret")]
+    public required string ClientSecret { get; set; }
+
+    [JsonPropertyName("redirect_uri")]
+    public required string RedirectUri { get; set; }
+}
---
+++ API/DTOs/GitHubInfo/GitHubEmail.cs
@@ -0,0 +1,8 @@
+﻿namespace API.DTOs.GitHubInfo;
+
+public class GitHubEmail
+{
+    public string Email { get; set; } = string.Empty;
+    public bool Primary { get; set; }
+    public bool Verified { get; set; }
+}
